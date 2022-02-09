14d2c65b5a96bb0f159a02496d26677deb5d60c6
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2022-02-09 06:27:33 +0300

    first commit

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import MovieSerializer
+from .models import Movie
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
